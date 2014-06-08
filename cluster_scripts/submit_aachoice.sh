ccde0d39eb245d0db03a057d7569fa9545c70488
sjspielman <stephanie.spielman@gmail.com>
2014-06-08 12:28:04 -0500

    submission changes. go from 4-12 aas

--- cluster_scripts/submit_aachoice.sh
+++ cluster_scripts/submit_aachoice.sh
@@ -1,24 +1,19 @@
-## SJS. shell script to vary numaa from 2 -> 10. starts with NUMAA=2
+## SJS. shell script to vary numaa from 4->12. starts with NUMAA=4
 
+# establish initial condition variable
+start=4
+
+# submit initial NUMAA=4 condition
 qsub aachoice.qsub
-sed -i "s/NUMAA=2/NUMAA=3/g" aachoice.qsub
-qsub aachoice.qsub
-sed -i "s/NUMAA=3/NUMAA=4/g" aachoice.qsub
-qsub aachoice.qsub
-sed -i "s/NUMAA=4/NUMAA=5/g" aachoice.qsub
-qsub aachoice.qsub
-sed -i "s/NUMAA=5/NUMAA=6/g" aachoice.qsub
-qsub aachoice.qsub
-sed -i "s/NUMAA=6/NUMAA=7/g" aachoice.qsub
-qsub aachoice.qsub
-sed -i "s/NUMAA=7/NUMAA=8/g" aachoice.qsub
-qsub aachoice.qsub
-sed -i "s/NUMAA=8/NUMAA=9/g" aachoice.qsub
-qsub aachoice.qsub
-sed -i "s/NUMAA=9/NUMAA=10/g" aachoice.qsub
-qsub aachoice.qsub
+
+# submit lots
+for ((i=4;i<12;i++)); do
+    next=$[i+1]
+    sed -i "s/NUMAA=$i/NUMAA=$next/g" aachoice.qsub 
+    qsub aachoice.qsub
+done
 
 # Reset
-sed -i "s/NUMAA=10/NUMAA=2/g" aachoice.qsub
+sed -i "s/NUMAA=$next/NUMAA=$start/g" aachoice.qsub
 
 
