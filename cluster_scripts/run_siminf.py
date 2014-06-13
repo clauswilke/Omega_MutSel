f5b893012f5c2febeee17ad3ed6d28306e9abee1
sjspielman <stephanie.spielman@gmail.com>
2014-06-12 20:36:48 -0500

    set default mu, bl

--- cluster_scripts/run_siminf.py
+++ cluster_scripts/run_siminf.py
@@ -1,6 +1,7 @@
 # SJS
 # Generic code for simulating and deriving omega via math, hyphy ML.
 # Be sure to cp src/ directory (simulator), hyphy files, and the functions_simandinf.py script into working directory
+# 6/12/14 -> best combo for 100000 sites where kappa=1 is mu=1e-6,bl=0.001
 
 import sys
 import numpy as np
@@ -29,42 +30,33 @@ seqlength = int(sys.argv[7])
 
 outfile = "params"+str(rep)+".txt"
 outf = open(outfile,'w')
-#outf.write('rep\tnumaa\taadist\tmu\tbl\tseqlength\tderived_w\thyphy_w\n')
+#outf.write('rep\tnumaa\taadist\tmu\tbl\tseqlength\tderived_w\tml_w\n')
 
 
-seqfile = "seq.fasta"
+seqfile = "seq"+str(rep)+".fasta"
 
-derived_w = None
+# Write tree given bl specifications
+treefile = "tree.tre"
+treef = open(treefile, 'w')
+treef.write("(t1:" + str(bl) + ", t2:" + str(bl) + ");")
+treef.close()
 
-bl_values = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5]
-mu_values = [1e-6, 1e-5, 1e-4, 1e-3]
+# Simulate
+print "simulating"
+f, aminos_used = setFreqs(aadist, numaa)
+simulate(f, seqfile, treefile, mu, seqlength, None) # omega is last arguement. when None, sim via mutsel
 
-for bl in bl_values:
-    # Write tree given bl specifications
-    treefile = "tree.tre"
-    treef = open(treefile, 'w')
-    treef.write("(t1:" + str(bl) + ", t2:" + str(bl) + ");")
-    treef.close()
-    
-    for mu in mu_values:
-     
-        # Simulate
-        print "simulating"
-        f, aminos_used = setFreqs(aadist, numaa)
-        simulate(f, seqfile, treefile, mu, seqlength, None) # omega is last arguement. when None, sim via mutsel
-            
-        # Derive omega
-        print "deriving"
-        if derived_w is None:
-            derived_w, num_codons = deriveOmega(f)
-        
-        # HyPhy/PAML omega
-        print "ML"
-        #ml_w = runhyphy("globalGY94.bf", "GY94_fixedkappa", seqfile, treefile, cpu)
-        ml_w = runpaml(seqfile, codonFreq = "0", initw = 0.4):
-        
-        # Save
-        outf.write(rep + '\t' + str(numaa) + '\t' + str(aadist) + '\t' + str(mu) + '\t' + str(bl) + '\t' + str(seqlength) + '\t' + str(derived_w) + '\t' + str(ml_w) + '\n')
+# Derive omega
+print "deriving"
+derived_w, num_codons = deriveOmega(f)
+
+# HyPhy/PAML omega
+print "ML"
+ml_w = runhyphy("globalGY94.bf", "GY94_fixedkappa", seqfile, treefile, cpu)
+#ml_w = runpaml(seqfile, "0")
+
+# Save
+outf.write(rep + '\t' + str(numaa) + '\t' + str(aadist) + '\t' + str(mu) + '\t' + str(bl) + '\t' + str(seqlength) + '\t' + str(derived_w) + '\t' + str(ml_w) + '\n')
 outf.close()
 
 
