9502ab392af0fb8f8184e6c19666fb671c3c8804
Stephanie J Spielman <stephanie.spielman@gmail.com>
2014-07-23 13:31:32 -0500

    16.tre and other changes

--- cluster_scripts/run_siminf.py
+++ cluster_scripts/run_siminf.py
@@ -5,45 +5,42 @@
 
 ######## Input parameters ########
 import sys
-if (len(sys.argv) != 6):
-    print "\n\nUsage: python run_siminf.py <rep> <mu> <bl> <seqlength> <simdir> \n."
+if (len(sys.argv) != 7):
+    print "\n\nUsage: python run_siminf.py <rep> <mu> <treefile> <seqlength> <simdir> <cpu> \n."
     sys.exit()
 rep = sys.argv[1]
 mu = float(sys.argv[2])
-bl = sys.argv[3]
+treefile = sys.argv[3]
 seqlength = int(sys.argv[4])
 simdir = sys.argv[5]
+cpu = sys.argv[6]
 sys.path.append(simdir)
 from functions_simandinf import *
 
-# So that half of simulations will have kappa~u(1,5) and half will have kappa=1.0
-if int(rep)%2 == 1:
-    kappa = rn.uniform(1.0, 5.0)
-else:
-    kappa = 1.0
 
-# parameter which affects the strength of selection pressure.
-lambda_ = rn.uniform(0.5, 3.5)
-
-
-
-# Write tree given bl specifications
-treefile = "tree.tre"
-treef = open(treefile, 'w')
-treef.write("(t1:" + str(bl) + ", t2:" + str(bl) + ");")
-treef.close()
 
 # set up output sequence and parameter files
 seqfile = "seqs"+str(rep)+".fasta"
 freqfile = "codonFreqs" + str(rep)+".txt"
 outfile = "params"+str(rep)+".txt"
 
+# More important parameters
+kappa = rn.uniform(1.0, 5.0) # kappa
+lambda_ = rn.uniform(0.5, 3.5) # sets strength of selection, effectively. This parameter will be the stddev for the normal distribution from which we draw scaled selection coefficients. Larger stddev = larger fitness differences among amino acids.
+
+
 # Simulate
 print "simulating"
-f_data, num_pref_aa, gc_content = setFreqs(freqfile, lambda_, 0.0, 1.0) # last 2 args are gc min, gc max
+f_data, gc_content = setFreqs(freqfile, lambda_, 0.0, 1.0) # last 2 args are gc min, gc max
 simulate(f_data, seqfile, treefile, mu, kappa, seqlength, None) # omega is last argument. when None, sim via mutsel
 
-# entropies and other frequencies
+
+# Derive omega
+print "deriving"
+mu_dict = {'AT':mu, 'AC':mu, 'AG':mu*kappa, 'CG':mu, 'CT':mu*kappa, 'GT':mu}
+derivedw = deriveOmega(f_data, mu_dict)
+
+# Calculate entropies and other frequencies for hyphy specification
 f_equal = np.zeros(61)
 f_equal[f_equal == 0.] = 1./61.
 f_f3x4 = calc_f3x4(f_data)
@@ -51,31 +48,19 @@ entropy_data = calcCodonEntropy(f_data)
 entropy_equal = 4.11087386417 # known, no need to calculate
 entropy_f3x4 = calcCodonEntropy(f_f3x4)
 
-# Derive omega
-print "deriving"
-mu_dict = {'AT':mu, 'AC':mu, 'AG':mu*kappa, 'CG':mu, 'CT':mu*kappa, 'GT':mu}
-derivedw = deriveOmega(f_data, mu_dict)
-
-
-# counting method omega
-neiw = run_neigojo(seqfile)
-
-# HyPhy omega and kappa, forever and ever
+# ML
 print "ML"
-ml_estimates = np.zeros(9)
-kappas = np.zeros(3)
-count = 0
-fspecs = ['equal', 'f3x4', 'data']
-kspecs = {1.:'one', kappa:'true', 'free':'free'}
+fspecs = {'equal':'equal_freqs', 'f3x4':'f3x4_freqs', 'data':'data_freqs'}
+kspecs = {kappa:'kappa_true', 'free':'kappa_free'}
 
-common_out_string = rep + '\t' + str(seqlength) + '\t' + str(bl) + '\t' + str(mu) + '\t' + str(kappa) + '\t' + str(gc_content) + '\t' + str(lambda_) + '\t' + str(entropy_data) + '\t' + str(entropy_f3x4) + '\t' + str(derivedw) + '\t' + str(neiw)
+common_out_string = rep + '\t' + str(seqlength) + '\t' + str(bl) + '\t' + str(mu) + '\t' + str(kappa) + '\t' + str(lambda_) + '\t' + str(gc_content) + '\t' + str(entropy_data) + '\t' + str(entropy_f3x4) + '\t' + str(derivedw)
 
 
 outf = open(outfile, 'w')
 for freqspec in fspecs:
-    freq = eval('f_'+freqspec)
+    hyfreq = eval('f_'+freqspec)
     for kapspec in kspecs:
-        mlw, mlk = runhyphy("globalDNDS.bf", "GY94", seqfile, treefile, "1.", kapspec, freq)
+        mlw, mlk = runhyphy("globalDNDS.bf", "GY94", seqfile, treefile, cpu, kapspec, hyfreq)
         w_err = (derivedw - mlw) / derivedw
         if mlk is not None:
             k_err = (kappa - mlk) / kappa
@@ -83,7 +68,7 @@ for freqspec in fspecs:
             # set these as numbers so R won't read those columns in as character.
             mlk = -10000
             k_err = -10000   
-        outf.write(common_out_string + '\t' + str(freqspec) + '\t' + str(kspecs[kapspec]) + '\t' + str(mlw) + '\t' + str(w_err) + '\t' + str(mlk) + '\t' + str(k_err) + '\n')
+        outf.write(common_out_string + '\t' + str(fspecs[freqspec]) + '\t' + str(kspecs[kapspec]) + '\t' + str(mlw) + '\t' + str(w_err) + '\t' + str(mlk) + '\t' + str(k_err) + '\n')
 outf.close()
 
 
