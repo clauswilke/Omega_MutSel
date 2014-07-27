6751677bfc11aa12932f4f823a5ecc30e68b2e43
sjspielman <stephanie.spielman@gmail.com>
2014-07-27 14:27:19 -0500

    haaaands

--- cluster_scripts/run_gy94.py
+++ cluster_scripts/run_gy94.py
@@ -18,26 +18,26 @@ outfile = "params"+str(rep)+".txt"
 seqlength = 50000
 omega = rn.uniform(0.01, 0.99) # omega
 kappa = rn.uniform(1.0, 5.0) # kappa
-lambda_ = rn.uniform(0.5, 3.5) # sets strength of selection, effectively. This parameter will be the stddev for the normal distribution from which we draw scaled selection coefficients. Larger stddev = larger fitness differences among amino acids.
+#lambda_ = rn.uniform(0.5, 3.5) # sets strength of selection, effectively. This parameter will be the stddev for the normal distribution from which we draw scaled selection coefficients. Larger stddev = larger fitness differences among amino acids.
 
 
 # Simulate
 print "simulating"
 mu=1. # there isn't such a parameter for gy94 so set to 1.
-f_data, gc_content = setFreqs(freqfile, lambda_, 0.0, 1.0) # last 2 args are gc min, gc max
-simulate(f_data, seqfile, treefile, mu, kappa, seqlength, omega) # omega is last argument. when None, sim via mutsel
-
+#f_data, gc_content = setFreqs(freqfile, lambda_, 0.0, 1.0) # last 2 args are gc min, gc max
 f_equal = np.zeros(61)
 f_equal[f_equal == 0.] = 1./61.
 
-mu_dict = {'AT':mu, 'AC':mu, 'AG':mu*kappa, 'CG':mu, 'CT':mu*kappa, 'GT':mu}
-derivedw = deriveOmega(f_data, mu_dict)
+simulate(f_equal, seqfile, treefile, mu, kappa, seqlength, omega) # omega is last argument. when None, sim via mutsel
 
-mlw_data, k = runhyphy("globalDNDS.bf", "GY94", seqfile, treefile, cpu, kappa, f_data)
-mlw_equal, k = runhyphy("globalDNDS.bf", "GY94", seqfile, treefile, cpu, kappa, f_equal)
+
+mu_dict = {'AT':mu, 'AC':mu, 'AG':mu*kappa, 'CG':mu, 'CT':mu*kappa, 'GT':mu}
+derivedw = deriveOmega(f_equal, mu_dict)
+
+mlw, k = runhyphy("globalDNDS.bf", "GY94", seqfile, treefile, cpu, kappa, f_equal)
 
 outf = open(outfile, 'w')
-outf.write(str(rep) + '\t' + str(lambda_) + '\t' + str(kappa) + '\t' + str(derivedw) + '\t' + str(omega) + '\t' + str(mlw_data) + '\t' + str(mlw_equal) +  '\n')
+outf.write(str(rep) + '\t' + str(kappa) + '\t' + str(derivedw) + '\t' + str(omega) + '\t' + str(mlw) +  '\n')
 outf.close()
 
 
